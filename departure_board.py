21e03486df82ca9dd7b1f90620c48fc9c00fdff5
rubenparnell <66206505+rubenparnell@users.noreply.github.com>
2025-02-01 20:53:06 +0000

    removed button

--- departure_board.py
+++ departure_board.py
@@ -57,16 +57,6 @@ def getTrains(stationCode, platform):
  #   logging.error("error fetching")
 
 
-def getButton():
-  with open("/home/ruben/button/toggle_file.txt", "r+") as f:
-    # Read the first line and strip any newline or whitespace
-    line = str(f.readline().strip())
-
-    # Convert to integer (or leave as a string, depending on your use case)
-    show = True if line == "1" else False
-  return show
-
-
 allTrains1 = []
 trains = getTrains(station1, platform1)[:2]
 for train in trains:
@@ -81,127 +71,120 @@ for train in trains:
 while True:
     current_time = int(time.time())
 
-    show = getButton()
+    lowestPixel = 1
+    # Clear screen:
+    image = Image.new("RGB", (matrix.width, matrix.height), (0,0,0))
+    draw = ImageDraw.Draw(image)
 
-    if show:
-        lowestPixel = 1
-        # Clear screen:
-        image = Image.new("RGB", (matrix.width, matrix.height), (0,0,0))
-        draw = ImageDraw.Draw(image)
+    if current_time % 30 == 0:
+        #print("loading...")
+        # Get train data
+        allTrains1 = []
+        trains = getTrains(station1, platform1)[:2]
+        for train in trains:
+            allTrains1.append((train['destination'], train['dueIn']))
 
-        if current_time % 30 == 0:
-            #print("loading...")
-            # Get train data
-            allTrains1 = []
-            trains = getTrains(station1, platform1)[:2]
-            for train in trains:
-                allTrains1.append((train['destination'], train['dueIn']))
-
-            allTrains2 = []
-            trains = getTrains(station2, platform2)[:2]
-            for train in trains:
-                allTrains2.append((train['destination'], train['dueIn']))
+        allTrains2 = []
+        trains = getTrains(station2, platform2)[:2]
+        for train in trains:
+            allTrains2.append((train['destination'], train['dueIn']))
 
 
-        # Display 1st info
-        draw.text((1, lowestPixel), f"{station1Name}: {platform1}", font=smallFont, fill=stationColour)
-        lowestPixel += smallFontHeight
+    # Display 1st info
+    draw.text((1, lowestPixel), f"{station1Name}: {platform1}", font=smallFont, fill=stationColour)
+    lowestPixel += smallFontHeight
 
-        for i, train in enumerate(allTrains1):
-            destination = train[0]
+    for i, train in enumerate(allTrains1):
+        destination = train[0]
 
-            if len(destination) > 15:
-                displayFont = smallFont
-                if i == 0:
-                    lowestPixel += 1
-            else:
-                displayFont = font
+        if len(destination) > 15:
+            displayFont = smallFont
+            if i == 0:
+                lowestPixel += 1
+        else:
+            displayFont = font
 
-            text_position = (1, lowestPixel)
+        text_position = (1, lowestPixel)
 
-            draw.text(text_position, destination, font=displayFont, fill=destinationColour)
+        draw.text(text_position, destination, font=displayFont, fill=destinationColour)
+        
+        due = str(train[1])
+        if due == "0":
+            due = "Due"
+        elif due == "-1":
+            due = "Due"
             
-            due = str(train[1])
-            if due == "0":
-                due = "Due"
-            elif due == "-1":
-                due = "Due"
-                
-            if len(destination) > 15:
-                text_position = (matrix.width-5*len(due), lowestPixel-1)
-            else:
-                text_position = (matrix.width-5*len(due), lowestPixel)
-                
-            draw.text(text_position, due, font=font, fill=destinationColour)
-
-            lowestPixel += font_size
+        if len(destination) > 15:
+            text_position = (matrix.width-5*len(due), lowestPixel-1)
+        else:
+            text_position = (matrix.width-5*len(due), lowestPixel)
             
-        if len(allTrains1) == 0:
-            lowestPixel += 2
-            text = "There are no services"
-            draw.text((int(matrix.width/2-(len(text)*4/2)), lowestPixel), text, font=smallFont, fill=destinationColour)
-            lowestPixel += smallFontHeight+1
-            
-            text = "from this platform"
-            draw.text((int(matrix.width/2-(len(text)*4/2)), lowestPixel), text, font=smallFont, fill=destinationColour)
+        draw.text(text_position, due, font=font, fill=destinationColour)
 
-        # Draw Line Separator:
-        lowestPixel = 24
-        line_position = (0, lowestPixel, matrix.width, lowestPixel)
+        lowestPixel += font_size
+        
+    if len(allTrains1) == 0:
         lowestPixel += 2
+        text = "There are no services"
+        draw.text((int(matrix.width/2-(len(text)*4/2)), lowestPixel), text, font=smallFont, fill=destinationColour)
+        lowestPixel += smallFontHeight+1
+        
+        text = "from this platform"
+        draw.text((int(matrix.width/2-(len(text)*4/2)), lowestPixel), text, font=smallFont, fill=destinationColour)
 
-        draw.line(line_position, fill=destinationColour, width=1)
+    # Draw Line Separator:
+    lowestPixel = 24
+    line_position = (0, lowestPixel, matrix.width, lowestPixel)
+    lowestPixel += 2
+
+    draw.line(line_position, fill=destinationColour, width=1)
 
 
-        # Display 2nd info:
-        draw.text((1, lowestPixel), f"{station2Name}: {platform2}", font=smallFont, fill=stationColour)
-        lowestPixel += smallFontHeight
+    # Display 2nd info:
+    draw.text((1, lowestPixel), f"{station2Name}: {platform2}", font=smallFont, fill=stationColour)
+    lowestPixel += smallFontHeight
 
-        for i, train in enumerate(allTrains2):
-            destination = train[0]
+    for i, train in enumerate(allTrains2):
+        destination = train[0]
 
-            if len(destination) > 15:
-                displayFont = smallFont
-                if i == 0:
-                    lowestPixel += 1
-            else:
-                displayFont = font
+        if len(destination) > 15:
+            displayFont = smallFont
+            if i == 0:
+                lowestPixel += 1
+        else:
+            displayFont = font
 
-            text_position = (1, lowestPixel)
+        text_position = (1, lowestPixel)
 
-            draw.text(text_position, destination, font=displayFont, fill=destinationColour)
+        draw.text(text_position, destination, font=displayFont, fill=destinationColour)
 
-            due = str(train[1])
-            if due == "0":
-                due = "Due"
-            elif due == "-1":
-                due = "Due"
+        due = str(train[1])
+        if due == "0":
+            due = "Due"
+        elif due == "-1":
+            due = "Due"
 
-            if len(destination) > 15:
-                text_position = (matrix.width-5*len(due), lowestPixel-1)
-            else:
-                text_position = (matrix.width-5*len(due), lowestPixel)
+        if len(destination) > 15:
+            text_position = (matrix.width-5*len(due), lowestPixel-1)
+        else:
+            text_position = (matrix.width-5*len(due), lowestPixel)
 
-            draw.text(text_position, due, font=font, fill=destinationColour)
+        draw.text(text_position, due, font=font, fill=destinationColour)
 
-            lowestPixel += font_size
+        lowestPixel += font_size
 
-        if len(allTrains1) == 0:
-            lowestPixel += 2
-            text = "There are no services"
-            draw.text((int(matrix.width/2-(len(text)*4/2)), lowestPixel), text, font=smallFont, fill=destinationColour)
-            lowestPixel += smallFontHeight+1
+    if len(allTrains1) == 0:
+        lowestPixel += 2
+        text = "There are no services"
+        draw.text((int(matrix.width/2-(len(text)*4/2)), lowestPixel), text, font=smallFont, fill=destinationColour)
+        lowestPixel += smallFontHeight+1
 
-            text = "from this platform"
-            draw.text((int(matrix.width/2-(len(text)*4/2)), lowestPixel), text, font=smallFont, fill=destinationColour)
+        text = "from this platform"
+        draw.text((int(matrix.width/2-(len(text)*4/2)), lowestPixel), text, font=smallFont, fill=destinationColour)
 
-        # Display the image
-        matrix.SetImage(image.convert('RGB'))
-    else:
-        #print("show is false")
-        image = Image.new("RGB", (matrix.width, matrix.height), (0,0,0))
-        draw = ImageDraw.Draw(image)
-        matrix.SetImage(image.convert('RGB'))
+    # Display the image
+    matrix.SetImage(image.convert('RGB'))
+
 
 
     time.sleep(1)
